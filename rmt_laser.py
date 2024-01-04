6a730066413cd02c6c010c02c372e2c44217afc7
Maxime Labonne <81252890+mlabonne@users.noreply.github.com>
2024-01-04 22:54:40 +0000

    Update rmt_laser.py

--- rmt_laser.py
+++ rmt_laser.py
@@ -1,27 +1,25 @@
-# %%
-model_name = "mlabonne/NeuralHermes-2.5-Mistral-7B"  # Change to your preferred model
-#model_name = "cognitivecomputations/dolphin-2.6-mistral-7b-dpo"  # Change to your preferred model
-
-
-# %%
+import argparse
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import datasets
-from lib.utils import gptq_data_utils
-from tqdm import tqdm
-import random
 import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from tqdm import tqdm
+from sklearn.utils.extmath import randomized_svd
+from lib.utils import gptq_data_utils
+
 
 class ModelModifier:
-    def __init__(self, model_name):
+    def __init__(self, model_name, svd_method):
         self.model_name = model_name
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map={"":0})
+        self.svd_method = svd_method
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype=torch.float16, device_map={"": 0}
+        )
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.original_weights = {}
         self.modified_layers = set()
         self.failed_attempts = set()
-        
 
+    # Method to update and reduce a model layer
     def update_model_reduce_layer(self, layer_type, layer_number):
         layer_id = f"{layer_type}_{layer_number}"
         if layer_id in self.modified_layers:
@@ -33,21 +31,40 @@ class ModelModifier:
                 print(f"Reconstructing layer: {name}")
                 original_dtype = module.weight.dtype
                 self.original_weights[name] = module.weight.detach().clone()
-                weights = module.weight.double()
-                U, S, V = torch.linalg.svd(weights, full_matrices=False)
+                weights = module.weight.float()
+
+                # Perform SVD or randomized SVD based on the specified method
+                if self.svd_method == "randomized_svd":
+                    weights_np = weights.cpu().detach().numpy()
+                    U, S, V = randomized_svd(
+                        weights_np,
+                        n_components=min(weights_np.shape),
+                        n_iter="auto",
+                        random_state=None,
+                    )
+                    # Convert U, S, V to PyTorch tensors
+                    S = torch.from_numpy(S).to(original_dtype).float()
+                    U = torch.from_numpy(U).to(original_dtype).float()
+                    V = torch.from_numpy(V).to(original_dtype).float()
+                else:
+                    U, S, V = torch.linalg.svd(weights, full_matrices=False)
 
                 # Estimate sigma using the full IQR method
                 sigma_estimated_full_iqr = self.estimate_sigma_with_full_iqr(S)
 
                 # Calculate Marchenko-Pastur threshold
                 n, m = weights.shape
-                mp_threshold_full_iqr = self.marchenko_pastur_threshold(sigma_estimated_full_iqr, n, m)
+                mp_threshold_full_iqr = self.marchenko_pastur_threshold(
+                    sigma_estimated_full_iqr, n, m
+                )
 
                 # Retain only the singular values above the MP threshold
                 S_reduced = torch.zeros_like(S)
                 k = (S > mp_threshold_full_iqr).sum().item()
                 S_reduced[:k] = S[:k]
                 print(f"Reduced from {S.shape} to {k}")
+
+                # S_reduced = S_reduced.to(original_dtype).float()
 
                 # Reconstruct the matrix using the thresholded singular values
                 reconstructed_weights = U @ torch.diag(S_reduced) @ V
@@ -59,9 +76,9 @@ class ModelModifier:
     @staticmethod
     def marchenko_pastur_threshold(sigma, n, m):
         beta = n / m if n < m else m / n
-        threshold = sigma * np.sqrt((1 + np.sqrt(beta))**2)
+        threshold = sigma * np.sqrt((1 + np.sqrt(beta)) ** 2)
         return threshold
-    
+
     ## Calculate an estimate of the standard deviation of the singular values based on Inter Quantile Range
 
     @staticmethod
@@ -69,9 +86,10 @@ class ModelModifier:
         q75 = torch.quantile(S, 0.75)
         q25 = torch.quantile(S, 0.25)
         iqr = q75 - q25
-        sigma_estimated = iqr / 1.349 ## 0.6745 * sigma is the expected range between the quantiles (Q1 and Q3)
+        sigma_estimated = (
+            iqr / 1.349
+        )  ## 0.6745 * sigma is the expected range between the quantiles (Q1 and Q3)
         return sigma_estimated
-
 
     def restore_model_original_layer(self, layer_type, layer_number):
         layer_id = f"{layer_type}_{layer_number}"
@@ -85,36 +103,51 @@ class ModelModifier:
                 else:
                     print(f"No original weights saved for layer: {name}")
 
-    def calculate_model_perplexity(self, datasets=['wikitext2', 'c4', 'ptb'], seqlen=384, use_cuda_graph=False, use_flash_attn=False):
+    def calculate_model_perplexity(
+        self,
+        datasets=["wikitext2", "c4", "ptb"],
+        seqlen=384,
+        use_cuda_graph=False,
+        use_flash_attn=False,
+    ):
         model = self.model
         model_str = self.model_name
         acc_loss = 0.0
         total_samples = 0
 
         for dataset in datasets:
-            input_tok = gptq_data_utils.get_test_tokens(dataset, seed=0, seqlen=seqlen, model=model_str)
+            input_tok = gptq_data_utils.get_test_tokens(
+                dataset, seed=0, seqlen=seqlen, model=model_str
+            )
             nsamples = input_tok.numel() // seqlen
-            input_tok = input_tok[0, :(seqlen * nsamples)].view(nsamples, seqlen)
+            input_tok = input_tok[0, : (seqlen * nsamples)].view(nsamples, seqlen)
             total_samples += nsamples
 
-            #if not use_cuda_graph:
+            # if not use_cuda_graph:
             #    model.reset()
 
             loss_fct = torch.nn.CrossEntropyLoss().cuda()
             progress = tqdm(range(nsamples))
             for ii in progress:
                 input = input_tok[ii, :].cuda().view(1, -1)
-                output = model(input, use_cache=False, output_hidden_states=False, output_attentions=False)[0]
+                output = model(
+                    input,
+                    use_cache=False,
+                    output_hidden_states=False,
+                    output_attentions=False,
+                )[0]
                 shift_logits = output[:, :-1, :].contiguous()
                 shift_labels = input[:, 1:]
-                loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+                loss = loss_fct(
+                    shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+                )
                 acc_loss += loss.item()
                 progress.set_description(f"avg_loss = {acc_loss/(ii+1)}")
 
         avg_loss = acc_loss / total_samples
         ppl = torch.exp(torch.tensor(avg_loss)).item()
         return ppl
-    
+
     ### Implement a Backward Search
     # Search for the optimal lower ranking approximations from the top layers downwards
     # Also, we try doing a greedy approach, in order to maximize the rank reduction.
@@ -124,9 +157,9 @@ class ModelModifier:
     def search_optimal_layer_modification(self, layer_types, layer_numbers, max_mod=5):
         # Calculate initial perplexity with original model weights
         initial_perplexity = self.calculate_model_perplexity()
-        print("="*50)
+        print("=" * 50)
         print(f"The initial perplexity of the model is {initial_perplexity}")
-        print("="*50)
+        print("=" * 50)
         min_loss = initial_perplexity
         optimal_params = (None, None)
         mods = 0
@@ -151,9 +184,11 @@ class ModelModifier:
                         optimal_params = (layer_type, layer_number)
                         mods = mods + 1
                         # Break out of the loop as soon as a better configuration is found
-                        print("*"*50)
-                        print(f"Improved perplexity found: {min_loss} for layer {layer_type} {layer_number}. Total modifications is {mods}")
-                        print("*"*50)
+                        print("*" * 50)
+                        print(
+                            f"Improved perplexity found: {min_loss} for layer {layer_type} {layer_number}. Total modifications is {mods}"
+                        )
+                        print("*" * 50)
                     else:
                         self.restore_model_original_layer(layer_type, layer_number)
                         self.failed_attempts.add(attempt)  # Record the failed attempt
@@ -165,35 +200,48 @@ class ModelModifier:
         return optimal_params, min_loss
 
     def save_model(self, save_dir):
-
         self.model.save_pretrained(save_dir)
         self.tokenizer.save_pretrained(save_dir)
 
-# Usage
-modifier = ModelModifier(model_name)
 
+if __name__ == "__main__":
+    # Parsing command-line arguments
+    parser = argparse.ArgumentParser(description="Modify LLM layers using SVD methods")
+    parser.add_argument("model_name", type=str, help="Name of the model")
+    parser.add_argument(
+        "--method",
+        type=str,
+        default="randomized_svd",
+        choices=["svd", "randomized_svd"],
+        help='SVD method to use: "svd" or "randomized_svd" (default: randomized_svd)',
+    )
 
-# %%
-## Sequence of tests
+    args = parser.parse_args()
 
-modifier.update_model_reduce_layer('mlp.down_proj', '25')
-modifier.update_model_reduce_layer('mlp.down_proj', '25')
-modifier.restore_model_original_layer('mlp.down_proj', '25')
-modifier.update_model_reduce_layer('mlp.down_proj', '25')
-modifier.restore_model_original_layer('mlp.down_proj', '25')
+    # Initialize ModelModifier with provided arguments
+    modifier = ModelModifier(args.model_name, args.method)
 
-# %%
-layers = list(range(31, -1, -1))
-layers = [f".{l}." for l in layers]
-print(layers)
+    # Sequence of tests
+    modifier.update_model_reduce_layer("mlp.down_proj", "25")
+    modifier.update_model_reduce_layer("mlp.down_proj", "25")
+    modifier.restore_model_original_layer("mlp.down_proj", "25")
+    modifier.update_model_reduce_layer("mlp.down_proj", "25")
+    modifier.restore_model_original_layer("mlp.down_proj", "25")
 
-# %%
-## Search and modify all layers
+    layers = list(range(31, -1, -1))
+    layers = [f".{l}." for l in layers]
+    print(layers)
 
-loop_check, min_loss = modifier.search_optimal_layer_modification(layer_types=['mlp.down_proj', 'mlp.up_proj', 'self_attn.q_proj', 'self_attn.k_proj', 'self_attn.v_proj', 'self_attn.o_proj'],
-                                    layer_numbers=layers)
-
-# %%
-modifier.save_model("laser_model")
-
-
+    ## Search and modify all layers
+    loop_check, min_loss = modifier.search_optimal_layer_modification(
+        layer_types=[
+            "mlp.down_proj",
+            "mlp.up_proj",
+            "self_attn.q_proj",
+            "self_attn.k_proj",
+            "self_attn.v_proj",
+            "self_attn.o_proj",
+        ],
+        layer_numbers=layers,
+    )
+    modifier.save_model("laser_model")
